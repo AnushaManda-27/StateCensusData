904ead2e3ef1b3064eeabc8fbc82dc58462211e1
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-15 21:51:34 +0530

    [Anusha] Add: Number of record check

---
+++ statedataTC1.1.py
@@ -0,0 +1,28 @@
+
+'''@Author: Anusha Manda
+@Date: 11-08-2021 12: 00: 30
+@Last Modified by: Anusha Manda
+@Last Modified time: 11-08-2021 18: 00: 30
+@Title: Reading csv files using Exceptions
+'''
+
+import StateCensusData_UC1
+import unittest
+import os
+os.chdir('C:\\Users\\M Veeraiah\\Desktop\\python_main\\Reading_writing_CSV')
+
+
+class TestStatecensus(unittest.TestCase, Exception):
+
+    
+
+    def test_no_of_records_happy_test_case(self):
+        '''Function: test_no_of_records_happy_test_case checks the given test case
+
+            Function paramerts: self
+
+            Return: none
+        '''
+        res1, res2 = StateCensusData_UC1.read('StateCensusData.csv')
+        res = StateCensusData_UC1.IndianStatesInfo.records(res1)
+        self.assertEqual(res, 29)
