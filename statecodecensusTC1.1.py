7b46a7063ff316b474e8e2ed05399be6104b1a9f
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-15 22:16:50 +0530

    [Anusha] Add: number of records

---
+++ statecodecensusTC1.1.py
@@ -0,0 +1,14 @@
+import pytest
+from StateCode_UC3 import StateCensusData, StateCensusAnalyzer, StateCode
+
+class TestCensusDataWithCode():
+
+    def test_records_census(self):
+        '''Function: test_record_census check for number of records mismatch
+
+            Function Parameters: self
+
+            Return: None'''
+        obj = StateCensusAnalyzer.read('StateCensusData.csv')
+        res = obj.records()
+        assert res == 29
