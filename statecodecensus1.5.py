7b01642e6593b52ac648104c2a8399f1748cd707
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-15 22:24:46 +0530

    [Anusha] Add: file name mismatch

---
+++ statecodecensus1.5.py
@@ -0,0 +1,46 @@
+import pytest
+from StateCode_UC3 import StateCensusData, StateCensusAnalyzer, StateCode
+
+class TestCensusDataWithCode():
+
+    def test_type_mismatch_census(self):
+        '''Function: test_type_mismatch_census check for type mismatch
+
+            Function Parameters: self
+
+            Return: None
+            '''
+        obj = StateCensusAnalyzer('StateCensusData.csv')
+        res = obj.type_mismatch()
+        assert res == None
+
+    def test_delimiter_census(self):
+        '''Function: test_delimiter_census check for delimiter mismatch
+
+            Function Parameters: self
+
+            Return: None'''
+        obj = StateCensusAnalyzer.read('StateCensusData.csv')
+        res = obj.detect_delimiter()
+        assert res == None
+
+    def test_header_census(self):
+        '''Function: test_header_census check for header mismatch
+
+            Function Parameters: self
+
+            Return: None
+            '''
+        obj = StateCensusAnalyzer.read('StateCensusData.csv')
+        res = obj.header_check()
+        assert res == None
+
+    def test_records_census(self):
+        '''Function: test_record_census check for number of records mismatch
+
+            Function Parameters: self
+
+            Return: None'''
+        obj = StateCensusAnalyzer.read('StateCensusData.csv')
+        res = obj.records()
+        assert res == 29
